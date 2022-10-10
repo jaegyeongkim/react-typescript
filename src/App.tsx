61813cfb2a945155f88de62162bfb6d17a8bc5d7
jaegyeongkim <tkwk1205@naver.com>
2022-10-10 21:22:01 +0900

    feat: theme 설정 (#1) 	modified:   src/App.tsx 	new file:   src/style/theme.ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import React from "react";
 import { RecoilRoot } from "recoil";
 import { QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { ThemeProvider } from "styled-components";
 
 import GlobalStyles from "style/globalstyle";
 
-import Router from "./router";
 import { queryClient } from "service/queryClient";
+import theme from "style/theme";
+import Router from "./router";
 
 const App = () => {
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools />
-        <GlobalStyles />
-        <Router />
+        <ThemeProvider theme={theme}>
+          <ReactQueryDevtools />
+          <GlobalStyles />
+          <Router />
+        </ThemeProvider>
       </QueryClientProvider>
     </RecoilRoot>
   );
