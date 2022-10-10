ef85f9f10cdf3ae62c14c5d03fd37b1078c21b9b
jaegyeongkim <tkwk1205@naver.com>
2022-10-11 00:57:37 +0900

    feat: Layout 수정 (#3) 	modified:   src/components/Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,9 @@ import styled from "styled-components";
 const Layout = () => {
   return (
     <Root>
-      <Outlet />
+      <Section>
+        <Outlet />
+      </Section>
     </Root>
   );
 };
@@ -13,6 +15,13 @@ const Layout = () => {
 export default Layout;
 
 const Root = styled.main`
+  display: flex;
+  justify-content: center;
   width: 100%;
   height: 100vh;
+  padding: 20px 0;
+`;
+
+const Section = styled.section`
+  width: 80%;
 `;
