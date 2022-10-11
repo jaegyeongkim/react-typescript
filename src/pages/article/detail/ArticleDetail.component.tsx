21628ebde63faeb7d25d980cfc973c1c522b9a79
jaegyeongkim <tkwk1205@naver.com>
2022-10-11 21:25:48 +0900

    feat: article detail router 연동 	new file:   src/pages/article/detail/ArticleDetail.component.tsx 	new file:   src/pages/article/detail/ArticleDetail.styled.ts 	modified:   src/pages/article/main/container/ArticleTable.tsx 	modified:   src/pages/index.ts 	modified:   src/router.tsx 	modified:   src/utils/constants.ts

---
+++ src/pages/article/detail/ArticleDetail.component.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { CommonH1 } from "style/commonStyled";
+
+const ArticleDetail = () => {
+  return (
+    <>
+      <CommonH1>Article Detail</CommonH1>
+    </>
+  );
+};
+
+export default ArticleDetail;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,7 @@
 import NotFound from "./notFound/NotFound";
 import Article from "./article/main/Article.component";
+import ArticleDetail from "./article/detail/ArticleDetail.component";
 import ArticleCreate from "./article/create/ArticleCreate.component";
 import ArticleUpdate from "./article/update/ArticleUpdate.component";
 
-export { NotFound, Article, ArticleCreate, ArticleUpdate };
+export { NotFound, Article, ArticleDetail, ArticleCreate, ArticleUpdate };
