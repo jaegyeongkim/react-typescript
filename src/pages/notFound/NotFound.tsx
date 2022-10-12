0c6aeb5086cb4efd0ad34aac145142d3451bd729
jaegyeongkim <tkwk1205@naver.com>
2022-10-13 00:30:49 +0900

    feat: NotFound 페이지 수정 (#11) 	modified:   src/hooks/index.ts 	new file:   src/hooks/useNotfound.ts 	modified:   src/pages/notFound/NotFound.tsx

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+import { CommonH1 } from "style/commonStyled";
+import { mixinBtn } from "style/mixin/button";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  const handleGotoHome = () => {
+    navigate("/");
+  };
+  return (
+    <section>
+      <CommonH1>NotFound</CommonH1>
+      <GotoBtn type="button" onClick={handleGotoHome}>
+        홈으로 가기
+      </GotoBtn>
+    </section>
+  );
 };
 
 export default NotFound;
+
+const GotoBtn = styled.button`
+  ${mixinBtn}
+`;
