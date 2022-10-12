bf43144b53783dc68430ab335742d7969c49d038
jaegyeongkim <tkwk1205@naver.com>
2022-10-13 00:06:33 +0900

    feat: load detail article (#11) 	modified:   src/pages/article/detail/ArticleDetail.component.tsx

--- src/pages/article/detail/ArticleDetail.component.tsx
+++ src/pages/article/detail/ArticleDetail.component.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link, useSearchParams } from "react-router-dom";
+import { useRecoilState } from "recoil";
 
+import { ArticleStorageState } from "store/persist";
 import { CommonH1 } from "style/commonStyled";
 import * as S from "./ArticleDetail.styled";
 
 const ArticleDetail = () => {
   const [searchParams] = useSearchParams();
+  const [articleStorage] = useRecoilState(ArticleStorageState);
+
+  const id = searchParams.get("id") || 0;
 
   return (
     <>
@@ -13,15 +18,15 @@ const ArticleDetail = () => {
       <S.Wrapper>
         <S.BtnWrapper>
           <button type="button">삭제</button>
-          <Link to={`/article/update?id=${searchParams.get("id")}`}>수정</Link>
+          <Link to={`/article/update?id=${id}`}>수정</Link>
         </S.BtnWrapper>
         <S.TitleWrapper>
           <S.Label>제목</S.Label>
-          <S.TitleBox></S.TitleBox>
+          <S.TitleBox>{articleStorage[id].title}</S.TitleBox>
         </S.TitleWrapper>
         <S.TitleWrapper>
           <S.Label>내용</S.Label>
-          <S.ContentBox></S.ContentBox>
+          <S.ContentBox>{articleStorage[id].content}</S.ContentBox>
         </S.TitleWrapper>
       </S.Wrapper>
     </>
